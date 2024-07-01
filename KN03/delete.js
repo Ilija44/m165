c81ed3fc6bc006ecd009fa362fd0669e90f26e6e
Ilija <113606362+Ilija44@users.noreply.github.com>
2024-07-01 13:30:22 +0200

    Update delete.js

--- KN03/delete.js
+++ KN03/delete.js
@@ -1,3 +1,6 @@
 db.kunden.deleteOne({ _id: ObjectId("CUSTOMER_ID_TO_DELETE") });
-db.bestellungen.deleteMany({ status: "abgeschlossen" });
+db.kunden.updateMany(
+    {},
+    { $pull: { bestellungen: { status: "abgeschlossen" } } }
+);
 db.produkte.deleteMany({ $or: [{ _id: ObjectId("PRODUCT_ID_1") }, { _id: ObjectId("PRODUCT_ID_2") }] });
